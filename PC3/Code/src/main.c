ccfaf441de1335229227c1f15a3beb9b5d80e7bb
Nunesamos <amosdossantosnunes@gmail.com>
2021-04-18 21:30:32 -0300

    Teste PC3

---
+++ PC3/Code/src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "../include/take5pic.h"
+
+int main(void){
+    
+    takeLoopPic();
+    printf("Tudo rodando \n");
+
+    return 0;
+}
